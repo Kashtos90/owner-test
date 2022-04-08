254e8a92d7379f3125728a026dab95c7d47e7ba3
Sergei Kashtuev <29061990a@gmail.com>
2022-04-08 16:29:00 +0700

    added corrections

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -2,8 +2,8 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({"system:properties", "classpath:local.properties", "classpath:remote.properties"})
+
+@Config.Sources({ "classpath:${typeProperties}.properties"})
 public interface WebDriverConfig extends Config{
 
     @Config.Key("browserName")
